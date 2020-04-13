af9a0f22f77a8d93f5a53c88bd13b401b65f25fe
HPVoid <henry.voigt1@gmx.de>
2020-04-13 17:52:46 +0200

    added method .attack and class variable damage (dictionary)

--- pokemon.py
+++ pokemon.py
@@ -2,6 +2,10 @@
 
 
 class Pokemon:
+
+    damage = {"tr": 2, "tg": 0.5, "tp": 0.5, "rt": 1, "rg": 2, "rp": 1, "gt": 1, "gr": 2, "gp": 0.5, "pt": 0.5, "pr": 2, "pg": 2}
+
+
     def __init__(self, name, level, type):
         self.name = name
         self.level = level
@@ -9,8 +13,6 @@ class Pokemon:
         self.health = 100 + (level-1)*10
         self.max_health = 100 + (level-1)*10
         self.is_ko = False
-
-
 
     def __repr__(self):
         return "Ein {} vom Typ {}.".format(self.name, self.type)
@@ -45,20 +47,61 @@ class Pokemon:
             self.health = 1
         print("{} is back!!!".format(self.name))
 
+    def attack(self, pokemon):
+        hits = 0
+        if self.type == "tuner" and pokemon.type == "raver":
+            hits = int(((self.level-1)*2+10)*self.damage["tr"])
+            pokemon.lose_health(hits)
+        if self.type == "tuner" and pokemon.type == "gamer":
+            hits = int(((self.level-1)*2+10)*self.damage["tg"])
+            pokemon.lose_health(hits)
+        if self.type == "tuner" and pokemon.type == "pumper":
+            hits = int(((self.level-1)*2+10)*self.damage["tp"])
+            pokemon.lose_health(hits)
+        if self.type == "raver" and pokemon.type == "tuner":
+            hits = int(((self.level-1)*2+10)*self.damage["rt"])
+            pokemon.lose_health(hits)
+        if self.type == "raver" and pokemon.type == "gamer":
+            hits = int(((self.level-1)*2+10)*self.damage["rg"])
+            pokemon.lose_health(hits)
+        if self.type == "raver" and pokemon.type == "pumper":
+            hits = int(((self.level-1)*2+10)*self.damage["rp"])
+            pokemon.lose_health(hits)
+        if self.type == "gamer" and pokemon.type == "tuner":
+            hits = int(((self.level-1)*2+10)*self.damage["gt"])
+            pokemon.lose_health(hits)
+        if self.type == "gamer" and pokemon.type == "raver":
+            hits = int(((self.level-1)*2+10)*self.damage["gr"])
+            pokemon.lose_health(hits)
+        if self.type == "gamer" and pokemon.type == "pumper":
+            hits = int(((self.level-1)*2+10)*self.damage["gp"])
+            pokemon.lose_health(hits)
+        if self.type == "pumper" and pokemon.type == "tuner":
+            hits = int(((self.level-1)*2+10)*self.damage["pt"])
+            pokemon.lose_health(hits)
+        if self.type == "pumper" and pokemon.type == "raver":
+            hits = int(((self.level-1)*2+10)*self.damage["pr"])
+            pokemon.lose_health(hits)
+        if self.type == "pumper" and pokemon.type == "gamer":
+            hits = int(((self.level-1)*2+10)*self.damage["pg"])
+            pokemon.lose_health(hits)
+        if self.type == pokemon.type:
+            hits = int(((self.level-1)*2+10))
+            pokemon.lose_health(hits)
+
+        print("{} dealt {} damage to {}".format(self.name, hits, pokemon.name))
 
 
 
 
-ronnymon = Pokemon("Ronnymon", 3, "ostdeutsch")
+ronnymon = Pokemon("Ronnymon", 3, "tuner")
+mikemon = Pokemon("Mikemon", 3, "raver")
+dennismon = Pokemon("Dennismon", 3, "gamer")
+mirkomon = Pokemon("Mirkomon", 3, "pumper")
 
 
-print(ronnymon)
-ronnymon.lose_health(15)
 
-ronnymon.gain_health(5)
 
-ronnymon.gain_health(80)
-
-ronnymon.lose_health(130)
-
-ronnymon.gain_health(50)
+ronnymon.attack(mikemon)
+ronnymon.attack(dennismon)
+ronnymon.attack(ronnymon)
